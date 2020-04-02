a4d18d6facb7c05bd406593ce23abeb98cd98344
nedpark <nedpark@outlook.com>
2020-04-02 10:07:05 +0900

    basic tags of html is added

--- website/js/seastack.js
+++ website/js/seastack.js
@@ -1,3 +1,28 @@
+/*
+    seastack.js
+
+    This source file is part of the Neuro Associastes web site project
+
+    Copyright (c) 2020 Neuro Associates Co., Ltd.
+    Licensed under MIT Licsense 
+*/
+
+'use strict'
+
+var seastack = {
+    init: function() {
+        this.findElements(document.getElementsByTagName("body"));
+    },
+
+    findElements: function(elements) {
+        if (!(elements instanceof HTMLCollection) || elements.length < 1) return;
+
+        console.log(elements);
+
+        console.log(elements)
+    }
+};
+
 window.onload = function() {
-    
+    this.seastack.init();
 }
